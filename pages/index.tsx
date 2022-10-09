2f7db66edd2358b358027db60da89429a4f741d3
Ubinquitous <ubinquitous1@gmail.com>
2022-10-09 21:14:40 +0900

    ADD : SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import ChartBox from '../components/ChartBox';
 import SEO from '../components/SEO';
 
 const App = () => {
   return (
-    <div>
-      <div className='app-wrap'>
-
-      </div>
-      <style jsx>{`
-        
-      `}</style>
+    <div className='app-wrap'>
+      <SEO>HOME</SEO>
+      <ChartBox />
     </div>
   );
 };
