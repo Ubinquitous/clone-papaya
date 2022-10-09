11b9adb555eb63e82c43f0b66b62e28dafeac411
Ubinquitous <ubinquitous1@gmail.com>
2022-10-09 20:43:51 +0900

    ADD : onSubmit, onChange 설정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { HeaderDiv, HeaderInput, HeaderInputWrap, HeaderSearch, HeaderText, HeaderWrap } from '../styles/Header';
 
 const Header = () => {
+    const [artist, setArtist] = useState('');
 
+    const onSubmit = useCallback((e: React.FormEvent) => {
+        e.preventDefault();
+    }, []);
 
+    const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setArtist(e.target.value);
+    }, []);
     return (
         <HeaderWrap>
             <HeaderDiv>
                 <HeaderText>Papaya music</HeaderText>
-                <HeaderInputWrap>
-                    <HeaderInput type='text' placeholder='Search your artist.' />
+                <HeaderInputWrap onSubmit={onSubmit}>
+                    <HeaderInput onChange={onChange} type='text' placeholder='Search your artist.' value={artist} />
                     <HeaderSearch>
                         <picture style={{ backgroundColor: 'transparent' }}>
                             <img src='https://cdn-icons-png.flaticon.com/512/149/149852.png' alt=''
