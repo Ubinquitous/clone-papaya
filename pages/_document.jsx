50c2e55e36aeb4e6ccb2cf01860b4b35c6ca87b7
Ubinquitous <ubinquitous1@gmail.com>
2022-10-09 20:25:20 +0900

    ADD : document

---
+++ pages/_document.jsx
@@ -0,0 +1,49 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+// Import styled components ServerStyleSheet
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        // Step 1: Create an instance of ServerStyleSheet
+        const sheet = new ServerStyleSheet();
+        const originalRenderpage = ctx.renderPage;
+
+        try {
+            // styled components SSR 하는 부분
+            // Step 2: Retrieve styles from components in the page
+            ctx.renderPage = () =>
+                originalRenderpage({
+                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } catch (error) {
+            console.error(error);
+        } finally {
+            sheet.seal();
+        }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
