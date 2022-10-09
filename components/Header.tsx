c1f8f8c3756efb5f2170dcbd73867cc7fae754de
Ubinquitous <ubinquitous1@gmail.com>
2022-10-10 02:45:15 +0900

    ADD : useRouter로 라우터명으로 값전달

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React, { useCallback, useState } from 'react';
 import { HeaderDiv, HeaderInput, HeaderInputWrap, HeaderSearch, HeaderText, HeaderWrap } from '../styles/Header';
 
 const Header = () => {
     const [artist, setArtist] = useState('');
+    const router = useRouter();
 
     const onSubmit = useCallback((e: React.FormEvent) => {
         e.preventDefault();
@@ -11,13 +15,19 @@ const Header = () => {
     const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setArtist(e.target.value);
     }, []);
+
+    const onSearchClick = () => {
+        router.push(`/detail/${artist}`)
+        setArtist('');
+    }
+
     return (
         <HeaderWrap>
             <HeaderDiv>
-                <HeaderText>Papaya music</HeaderText>
+                <HeaderText><Link href='/'><a style={{ backgroundColor: 'transparent' }}>Papaya music</a></Link></HeaderText>
                 <HeaderInputWrap onSubmit={onSubmit}>
                     <HeaderInput onChange={onChange} type='text' placeholder='Search your artist.' value={artist} />
-                    <HeaderSearch>
+                    <HeaderSearch onClick={onSearchClick}>
                         <picture style={{ backgroundColor: 'transparent' }}>
                             <img src='https://cdn-icons-png.flaticon.com/512/149/149852.png' alt=''
                                 style={{
