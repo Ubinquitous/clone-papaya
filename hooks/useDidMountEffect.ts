4cf3ff7cd5a1ccfcb8db71b3747b563b000ac824
Ubinquitous <ubinquitous1@gmail.com>
2022-10-10 16:32:42 +0900

    ADD : custom hooks

---
+++ hooks/useDidMountEffect.ts
@@ -0,0 +1,12 @@
+import React, { FunctionComponent, useEffect, useRef } from 'react';
+
+const useDidMountEffect = (func: any, deps: any) => {
+    const didMount = useRef(false);
+
+    useEffect(() => {
+        if (didMount.current) func();
+        else didMount.current = true;
+    }, deps);
+};
+
+export default useDidMountEffect;
